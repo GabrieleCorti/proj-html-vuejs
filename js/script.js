f773774eac9d02a3d4769434194bac3333c5f46c
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-04-19 17:51:04 +0200

    sezione work generata con vue, terminata

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@ const App = new Vue({
     
     el: "#app",
     data: {
-
+        imgNumbers: [{
+            imgNumber: "1-1",
+            description: "qualcosa"
+        },
+        {
+            imgNumber: "2",
+            description: "qualcosa"
+        },
+        {
+            imgNumber: "3-1",
+            description: "qualcosa"
+        },
+        {
+            imgNumber: "4-1",
+            description: "qualcosa"
+        },
+        {
+            imgNumber: "5-1",
+            description: "qualcosa"
+        },
+        {
+            imgNumber: "6-1",
+            description: "qualcosa"
+        }]
     },
     methods: {
         
